d7bbad2b5c6bc83625769b572927762746f48759
Mai Trung Hieu <hieumt211299@gmail.com>
2023-06-27 22:10:49 +0700

    update color

--- src/components/BoardGameRow.vue
+++ src/components/BoardGameRow.vue
@@ -1,27 +1,33 @@
 <script setup lang="ts">
-import BoardGameCol from './BoardGameCol.vue'
+import BoardGameCol from "./BoardGameCol.vue";
 
 interface Props {
-    row: string[],
-    indexRow:number,
-    boards:{}
+  row: string[];
+  indexRow: number;
+  boards: {};
 }
 
 const props = defineProps<Props>();
 interface Emits {
-    (event: "handleClick", indexCol:number,indexRow:number): void
+  (event: "handleClick", indexCol: number, indexRow: number): void;
 }
-const emit = defineEmits<Emits>()
+const emit = defineEmits<Emits>();
 
 const handleClick = (indexCol) => {
-    emit('handleClick', indexCol,props.indexRow)
-}
+  emit("handleClick", indexCol, props.indexRow);
+};
 </script>
 <template>
-    
-    <div class="flex border-collapse">
-    <BoardGameCol v-for="(col, index) in props.row" :key="index"  :col="col" :indexCol="index" :indexRow="indexRow" :boards="boards" @handle-click="handleClick"></BoardGameCol>
-
-    </div>
+  <div class="flex border-collapse">
+    <BoardGameCol
+      v-for="(col, index) in props.row"
+      :key="index"
+      :col="col"
+      :indexCol="index"
+      :indexRow="indexRow"
+      :boards="boards"
+      @handle-click="handleClick"
+    ></BoardGameCol>
+  </div>
 </template>
 <style></style>
--- src/components/PlayerVsPlayer.vue
+++ src/components/PlayerVsPlayer.vue
@@ -1,109 +1,176 @@
 <script setup lang="ts">
-import { ref, provide } from 'vue'
-import  BoardGameRow  from "./BoardGameRow.vue";
+import { ref, provide } from "vue";
+import BoardGameRow from "./BoardGameRow.vue";
 
 interface Emits {
-    (event: "handleClick"): void
+  (event: "handleClick"): void;
 }
-const emit = defineEmits<Emits>()
-
-const player1=ref<boolean>(true)
-
+const emit = defineEmits<Emits>();
+const player1 = ref<boolean>(true);
 const boards = ref<{}>([]);
+const winner = ref<string>("");
 // let data = "";
 // let size= ref<number>(3)
-for (let i = 0; i < 15; i++) {
-    boards.value[i] = new Array('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+for (let i = 0; i < 20; i++) {
+  boards.value[i] = new Array(
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
+  );
 }
 
-const handleClick = (indexCol,indexRow) => {
-    console.log(indexRow, indexCol);
-    if(boards.value[indexRow][indexCol]===''){
-        if (player1.value) {
-            boards.value[indexRow][indexCol] = 'X'
+const handleClick = (indexCol, indexRow) => {
+  if (boards.value[indexRow][indexCol] === "") {
+    if (player1.value) {
+      boards.value[indexRow][indexCol] = "X";
+    } else {
+      boards.value[indexRow][indexCol] = "O";
+    }
+    player1.value = !player1.value;
+  }
+  console.log(indexRow, indexCol);
+  // hang ngang
+  for (let i = 1; i <= 4; i++) {
+    if (
+      boards.value[indexRow][indexCol] === boards.value[indexRow][indexCol + i]
+    ) {
+      if (i == 4) {
+        if (
+          boards.value[indexRow][indexCol] ===
+          boards.value[indexRow][indexCol + i]
+        ) {
+          console.log("win");
+        }
+      }
+    } else {
+      for (let j = 1; j <= 4; j++) {
+        if (
+          boards.value[indexRow][indexCol + i - 1] ===
+          boards.value[indexRow][indexCol + i - 1 - j]
+        ) {
+          if (j == 4) {
+            if (
+              boards.value[indexRow][indexCol + i - 1] ===
+              boards.value[indexRow][indexCol + i - 1 - j]
+            ) {
+              console.log("win");
+            }
+          }
         } else {
-            boards.value[indexRow][indexCol] = 'O'
+          break;
         }
-        player1.value = !player1.value  
+      }
     }
-    console.log(indexRow,indexCol);
-// hang ngang
-    for (let i = 1; i <= 4; i++) {
-        if (boards.value[indexRow][indexCol] === boards.value[indexRow][indexCol + i]) {
-            if (i == 4) {
-                if (boards.value[indexRow][indexCol] === boards.value[indexRow][indexCol + i]) {
-                    console.log('win');
-                }
+  }
+  // hang doc
+  for (let i = 1; i <= 4; i++) {
+    if (
+      boards.value[indexRow][indexCol] === boards.value[indexRow + i][indexCol]
+    ) {
+      if (i == 4) {
+        if (
+          boards.value[indexRow][indexCol] ===
+          boards.value[indexRow + i][indexCol]
+        ) {
+          console.log("win");
+        }
+      }
+    } else {
+      for (let j = 1; j <= 4; j++) {
+        if (
+          boards.value[indexRow + i - 1][indexCol] ===
+          boards.value[indexRow + i - 1 - j][indexCol]
+        ) {
+          if (j == 4) {
+            if (
+              boards.value[indexRow + i - 1][indexCol] ===
+              boards.value[indexRow + i - 1 - j][indexCol]
+            ) {
+              console.log("win");
             }
+          }
         } else {
-            for (let j = 1; j <= 4; j++) {
-                if (boards.value[indexRow][indexCol + i - 1] === boards.value[indexRow][indexCol + i - 1 - j]) {
-                    if (j == 4) {
-                        if (boards.value[indexRow][indexCol + i - 1] === boards.value[indexRow][indexCol + i - 1 - j]) {
-                            console.log('win');
-                        }
-                    }
-
-                } else {
-                    break
-                }
-            }
+          break;
         }
-
+      }
     }
-// hang doc
-    for (let i = 1; i <= 4; i++) {
-        if (boards.value[indexRow][indexCol] === boards.value[indexRow+i][indexCol]) {
-            if (i == 4) {
-                if (boards.value[indexRow][indexCol] === boards.value[indexRow+i][indexCol]) {
-                    console.log('win');
-                }
+  }
+  // duong cheo
+  for (let i = 1; i <= 4; i++) {
+    if (
+      boards.value[indexRow][indexCol] ==
+      boards.value[indexRow + i][indexCol - i]
+    ) {
+      if (i == 4) {
+        if (
+          boards.value[indexRow][indexCol] ==
+          boards.value[indexRow + i][indexCol - i]
+        ) {
+          console.log("win");
+        }
+      }
+    } else {
+      for (let j = 1; j <= 4; j++) {
+        if (
+          boards.value[indexRow + i - 1][indexCol - i + 1] ==
+          boards.value[indexRow + i - 1 - j][indexCol - i + 1 + j]
+        ) {
+          if (j == 4) {
+            if (
+              boards.value[indexRow + i - 1][indexCol - i + 1] ==
+              boards.value[indexRow + i - 1 - j][indexCol - i + 1 + j]
+            ) {
+              console.log("win");
             }
+          }
         } else {
-            for (let j = 1; j <= 4; j++) {
-                if (boards.value[indexRow + i - 1][indexCol] === boards.value[indexRow + i - 1 - j][indexCol]) {
-                    if (j == 4) {
-                        if (boards.value[indexRow + i - 1][indexCol] === boards.value[indexRow + i - 1 - j][indexCol]) {
-                            console.log('win');
-                        }
-                    }
-
-                } else {
-                    break
-                }
-            }
+          break;
         }
-
+      }
     }
-// duong cheo
-    for(let i=1;i<=4;i++){
-        if (boards.value[indexRow][indexCol]==boards.value[indexRow+i][indexCol-i]){
-            if(i==4){
-                if(boards.value[indexRow][indexCol] == boards.value[indexRow + i][indexCol - i]){
-                    console.log('win');
-                }
-            }
-        }
-        else{
-            for(let j=1;j<=4;j++){
-                if (boards.value[indexRow + i - 1][indexCol - i + 1]==boards.value[indexRow + i - 1-j][indexCol - i + 1+j]){
-                    if(j==4){
-                        if(boards.value[indexRow + i - 1][indexCol - i + 1] == boards.value[indexRow + i - 1 - j][indexCol - i + 1 + j]){
-                            console.log('win');
-                        }
-                    }
-                }else{
-                    break
-                }
-            }
-
-
-        }
-    }
-}
-
+  }
+};
 </script>
 <template>
-    <BoardGameRow v-for="(row, index) in boards" :key="index" :row="row" :indexRow="index" :boards="boards" @handle-click="handleClick"></BoardGameRow>
+  <div class="overflow-hidden">
+    <BoardGameRow
+      v-for="(row, index) in boards"
+      :key="index"
+      :row="row"
+      :indexRow="index"
+      :boards="boards"
+      @handle-click="handleClick"
+    ></BoardGameRow>
+  </div>
 </template>
 <style></style>
