cf0613aef2eb4d6c278d3fe235b4b416cb7ef85a
mai trung hieu <maitrunghieu@MTH-MacBook-Pro.local>
2023-11-09 16:29:19 +0700

    change img

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-
 interface Emits {
   (event: "handleClickPVP"): void;
 }
 const emit = defineEmits<Emits>();
 const handle1vs1 = () => {
-  emit('handleClickPVP');
+  emit("handleClickPVP");
 };
 const handle1vsbot = () => {
   console.log("1vs bot");
@@ -16,7 +15,9 @@ const handlebotvsbot = () => {
 </script>
 
 <template>
-  <div class="flex items-center justify-center flex-col bg-[#B3C890]  rounded-lg">
+  <div
+    class="flex items-center justify-center flex-col bg-[#B3C890] rounded-lg"
+  >
     <!-- main card -->
     <div class="p-10 rounded-xl">
       <!-- headers content-->
@@ -27,22 +28,28 @@ const handlebotvsbot = () => {
       <!-- selecet mode -->
       <div class="grid grid-cols-1 gap-10 md:grid-cols-3 mt-10">
         <div class="bg-[#FFFBEC] rounded-xl">
-          <div class="flex flex-col p-8 rounded-xl bg-[#DBDFAA] shadow-xl translate-x-4 translate-y-4 w-96 md:w-auto">
+          <div
+            class="flex flex-col p-8 rounded-xl bg-[#DBDFAA] shadow-xl translate-x-4 translate-y-4 w-96 md:w-auto"
+          >
             <div class="flex items-center justify-evenly">
               <img src="../assets/imgs/gamer.png" class="w-8" />
               <img src="../assets/imgs/vs.png" class="w-8" />
               <img src="../assets/imgs/gamer.png" class="w-8" />
             </div>
             <div class="mt-3 font-semibold text-lg">Player VS Player</div>
-            <button class="bg-[#F4F5FA] px-4 py-3 rounded-full border border-[#F0F0F6] shadow-xl mt-4"
-              @click="handle1vs1">
+            <button
+              class="bg-[#F4F5FA] px-4 py-3 rounded-full border border-[#F0F0F6] shadow-xl mt-4"
+              @click="handle1vs1"
+            >
               Play
             </button>
           </div>
         </div>
 
         <div class="bg-[#FFFBEC] rounded-xl">
-          <div class="flex flex-col p-8 rounded-xl bg-[#DBDFAA] shadow-xl translate-x-4 translate-y-4 w-96 md:w-auto">
+          <div
+            class="flex flex-col p-8 rounded-xl bg-[#DBDFAA] shadow-xl translate-x-4 translate-y-4 w-96 md:w-auto"
+          >
             <div class="flex items-center justify-evenly">
               <img src="../assets/imgs/gamer.png" class="w-8" />
               <img src="../assets/imgs/vs.png" class="w-8" />
@@ -55,23 +62,31 @@ const handlebotvsbot = () => {
                               <span class="font-light text-sm">/month</span>
                           </div> -->
 
-            <button class="bg-[#F4F5FA] px-4 py-3 rounded-full border border-[#F0F0F6] shadow-xl mt-4"
-              @click="handle1vsbot">
+            <button
+              class="bg-[#d8d8d8] px-4 py-3 rounded-full border border-[#d8d8d8] shadow-xl mt-4 cursor-not-allowed"
+              disabled
+              @click="handle1vsbot"
+            >
               Play
             </button>
           </div>
         </div>
 
         <div class="bg-[#FFFBEC] rounded-xl">
-          <div class="flex flex-col p-8 rounded-xl bg-[#DBDFAA] shadow-xl translate-x-4 translate-y-4 w-96 md:w-auto">
+          <div
+            class="flex flex-col p-8 rounded-xl bg-[#DBDFAA] shadow-xl translate-x-4 translate-y-4 w-96 md:w-auto"
+          >
             <div class="flex items-center justify-evenly">
               <img src="../assets/imgs/bot.png" class="w-8" />
               <img src="../assets/imgs/vs.png" class="w-8" />
               <img src="../assets/imgs/bot.png" class="w-8" />
             </div>
             <div class="mt-3 font-semibold text-lg">Bot VS Bot</div>
-            <button class="bg-[#F4F5FA] px-4 py-3 rounded-full border border-[#F0F0F6] shadow-xl mt-4"
-              @click="handlebotvsbot">
+            <button
+              class="bg-[#d8d8d8] px-4 py-3 rounded-full border border-[#d8d8d8] shadow-xl mt-4 cursor-not-allowed"
+              @click="handlebotvsbot"
+              disabled
+            >
               Play
             </button>
           </div>
